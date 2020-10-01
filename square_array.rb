8582f594b8daa8175b199a7cff5b2a4b24621a15
bellaappel <bellaappel0@gmail.com>
2020-10-01 17:03:04 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
-def square_array(array)
+def square_array(numbers)
   # your code here
+  #what is the goal of this method 
+  #take the argument, which is an array of numbers, and return a new array of numbers, which are the numbers from the original array, but squared 
+  # test input [1, 2, 3]
+  # test output [1, 4, 9]
+
+  #what goes inside of the pipes is the name of a variable that should be the SINGULAR VERSION OF WHAT WE'RE ITERATING OVER
+  squared_numbers = []
+  numbers.each{|number| squared_numbers.push(number**2)}
+  squared_numbers
 end
